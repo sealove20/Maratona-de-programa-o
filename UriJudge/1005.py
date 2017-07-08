e9476bd39cd93040b1e609345fd96560dabed340
MHFS20 <matheus.181137@gmail.com>
2017-07-07 21:29:21 -0300

    problema de sexta 07/07/2017

---
+++ UriJudge/1005.py
@@ -0,0 +1,4 @@
+a = float(input())
+b = float(input())
+media = ((a*3.50)+(b*7.5))/11
+print("MEDIA = {0:.5f}".format(media))
--- UriJudge/1255.py
+++ UriJudge/1255.py
@@ -1,35 +1,26 @@
-n = int(input())
-palavra = ''
-maior = 0
-for i in range(n):
-    texto = input()
-    if len(texto) < 200: #verifíca a quantidade máxima de caracteres
-        for j in range(len(texto)):
-            cont=0
-            for k in range(len(texto)):
-                letra = texto[j].lower()
-                if letra == texto[k]:
-                    cont +=1
+import string
 
-            if j == 0:
-                contMaior = cont
+alf = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+       'w', 'x', 'y', 'z']
 
-            #verificação do caracter com maior número de ocorrencias na string
-            if cont > contMaior:
-                maior = letra
-                contMaior = cont
-                palavra = ''
-                palavra = maior
-            elif cont == contMaior:
-                palavra += letra
-                contMaior = cont
+#Entrada de dados
+tam = int(input())
 
-        # listaNova=''
-        # for k in range(len(lista)-1):
-        #     if k == 0:
-        #         letraCmp = lista[0]
-        #     elif lista[k] != lista[k+1]:
-        #         listaNova += lista[k]
+#função que conta a frequencia de aparacição das letras e adiciona em uma lista
+def conta(str, freq):
+    for x in range(len(alf)):
+        freq.append(str.count(alf[x]))
 
-        print(palavra)
 
+for k in range(tam):
+
+    n = input()
+    freq = []
+    str = n.lower()#função para alterar a string para caixa baixa
+    conta(str, freq)
+    m = max(freq)#função que retorna o valor máximo da lista
+
+    for i in range(len(alf)):
+        if freq[i] is m:
+            print(alf[i], end='')
+    print()
